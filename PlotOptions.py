ae3639504e0afe701d82b9c700f2ef19e731dc07
michaelhb <mhbardsley@gmail.com>
2015-12-18 18:56:56 +1100

    Changed order of params in plot_options to be a bit more sensible

--- PlotOptions.py
+++ PlotOptions.py
@@ -12,18 +12,18 @@ plot_options = namedtuple("plot_options", (
     "xindex",       # Index of x axis data
     "yindex",       # Index of y axis data
     "zindex",       # Index of z axis data
-    "xlabel",       # Label for x axis
-    "ylabel",       # Label for y axis
-    "zlabel",       # Label for z axis
-    "size",         # Size in inches [x, y]
-    "xticks",       # Number of x ticks
-    "yticks",       # Number of y ticks
-    "plottitle",    # Title of plot
-    "legtitle",     # Plot legend
-    "plot_limits",  # Plot limits [xmin, xmax, ymin, ymax]
-    "nbins",        # Number of bins
-    "bin_limits",   # Bin limits [[xmin, xmax], [ymin, ymax]]
     "logx",         # Apply log scale to xdata
     "logy",         # Apply log scale to ydata
     "logz",         # Apply log scale to zdata
+    "plot_limits",  # Plot limits [xmin, xmax, ymin, ymax]
+    "bin_limits",   # Bin limits [[xmin, xmax], [ymin, ymax]]    
+    "nbins",        # Number of bins    
+    "xticks",       # Number of x ticks
+    "yticks",       # Number of y ticks    
+    "size",         # Size in inches [x, y]    
+    "xlabel",       # Label for x axis
+    "ylabel",       # Label for y axis
+    "zlabel",       # Label for z axis
+    "plottitle",    # Title of plot
+    "legtitle",     # Plot legend
     ))

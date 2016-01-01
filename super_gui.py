0e7cd76041f9601af2957fc70b772939fc8aea43
michaelhb <mhbardsley@gmail.com>
2016-01-02 10:53:19 +1100

    Rearranged GUI code for readability.
    Main change is that all the table layout code
    (self.gridbox.attach etc) has been moved to the end,
    and comments / section markers have been added.

--- super_gui.py
+++ super_gui.py
@@ -170,44 +170,19 @@ class GUIControl:
         for plot_class in plots.plot_types:
             self.plots[plot_class.description] = plot_class
 
-        #######################################################################
+        # Plot type selection #################################################
 
-        # Make main GUI window.
-        self.window = gtk.Window()
-        self.window.maximize()  # Window is maximised.
-        self.window.set_title("SuperPlot")  # With this title in the header.
-        self.window.connect(
-                'destroy',
-                lambda w: gtk.main_quit())  # Quit when we press cross.
-
-        # Add a table of boxes to the window.
-        self.gridbox = gtk.Table(
-                16,
-                5,
-                False)  # 9 rows and 4 columns that are NOT homegenous.
-        self.window.add(self.gridbox)  # Add table to our window.
-        self.gridbox.show()  # Show the table.
-
-        #######################################################################
-
-        # Box to select  type of plot.
-        # Title.
-        typetitle = gtk.Button("Plot type:")
-        self.gridbox.attach(typetitle, 0, 1, 0, 1, xoptions=gtk.FILL)
         # Combo-box for various plot types.
+        typetitle = gtk.Button("Plot type:")
         self.typebox = gtk.combo_box_new_text()
-        self.gridbox.attach(self.typebox, 1, 2, 0, 1, xoptions=gtk.FILL)
         for description in self.plots.keys():
             self.typebox.append_text(description)
-        # typebox.connect('changed', self.ctype)
         self.typebox.set_active(default_plot_type)  # Set to default plot type.
 
-        #######################################################################
+        # X-Axis ##############################################################
 
-        # x-axis.
         # Name of variable.
         xtitle = gtk.Button("x-axis variable:")
-        self.gridbox.attach(xtitle, 0, 1, 1, 2, xoptions=gtk.FILL)
         # Combo-box for variables.
         self.x = gtk.combo_box_new_text()
         # List the available parameter names in a particlular order
@@ -218,22 +193,21 @@ class GUIControl:
                     '')  # Remove $ from GUI, but not from plot labels.
             self.x.append_text(data_name)
         self.x.set_wrap_width(5)  # Make box wider for long lists.
-        self.gridbox.attach(self.x, 1, 2, 1, 2, xoptions=gtk.FILL)
         self.x.connect('changed', self._cx)
 
         # Text box to alter x-axis label.
         self.xtext = gtk.Entry()
         self.xtext.set_text(self.labels[self.xindex])
-        self.gridbox.attach(self.xtext, 1, 2, 2, 3, xoptions=gtk.FILL)
         self.xtext.connect("changed", self._cxtext)
 
         # Set default plot variable.
         self.x.set_active(self.xindex)
 
+        # Y-Axis ##############################################################
+
         # y-axis.
         # Name of variable.
         ytitle = gtk.Button("y-axis variable:")
-        self.gridbox.attach(ytitle, 0, 1, 3, 4, xoptions=gtk.FILL)
         # Combo-box for variables.
         self.y = gtk.combo_box_new_text()
         # List the available parameter names in a particlular order
@@ -244,22 +218,19 @@ class GUIControl:
                     '')  # Remove $ from GUI, but not from plot labels.
             self.y.append_text(data_name)
         self.y.set_wrap_width(5)  # Make box wider for long lists.
-        self.gridbox.attach(self.y, 1, 2, 3, 4, xoptions=gtk.FILL)
         self.y.connect('changed', self._cy)
 
         # Text box to alter y-axis label.
         self.ytext = gtk.Entry()
         self.ytext.set_text(self.labels[self.yindex])
-        self.gridbox.attach(self.ytext, 1, 2, 4, 5, xoptions=gtk.FILL)
         self.ytext.connect("changed", self._cytext)
 
         # Set default plot variable.
         self.y.set_active(self.yindex)
 
-        # z-axis.
+        # Z-Axis ##############################################################
         # Name of variable.
         ztitle = gtk.Button("z-axis variable:")
-        self.gridbox.attach(ztitle, 0, 1, 5, 6, xoptions=gtk.FILL)
         # Combo-box for variables.
         self.z = gtk.combo_box_new_text()
         # List the available parameter names in a particlular order
@@ -270,118 +241,144 @@ class GUIControl:
                     '')  # Remove $ from GUI, but not from plot labels.
             self.z.append_text(data_name)
         self.z.set_wrap_width(5)  # Make box wider for long lists.
-        self.gridbox.attach(self.z, 1, 2, 5, 6, xoptions=gtk.FILL)
         self.z.connect('changed', self._cz)
 
         # Text box to alter z-axis label.
         self.ztext = gtk.Entry()
         self.ztext.set_text(self.labels[self.zindex])
-        self.gridbox.attach(self.ztext, 1, 2, 6, 7, xoptions=gtk.FILL)
         self.ztext.connect("changed", self._cztext)
 
         # Set default plot variable.
         self.z.set_active(self.zindex)
 
-        #######################################################################
-
-        # Check-boxes to indicate whether data should be logged.
+        # Log Scaling #########################################################
 
         self.logx = gtk.CheckButton('Log x-data.')
-        self.gridbox.attach(self.logx, 0, 1, 2, 3, xoptions=gtk.FILL)
         self.logy = gtk.CheckButton('Log y-data.')
-        self.gridbox.attach(self.logy, 0, 1, 4, 5, xoptions=gtk.FILL)
         self.logz = gtk.CheckButton('Log z-data.')
-        self.gridbox.attach(self.logz, 0, 1, 6, 7, xoptions=gtk.FILL)
 
-        #######################################################################
+        # Plot Title ##########################################################
 
-        # Text boxes for titles.
-
-        # Main title.
         tplottitle = gtk.Button("Plot title:")
-        self.gridbox.attach(tplottitle, 0, 1, 9, 10, xoptions=gtk.FILL)
-        # Text box to alter title.
         self.plottitle = gtk.Entry()
         self.plottitle.set_text(default("plot_title"))
-        self.gridbox.attach(self.plottitle, 1, 2, 9, 10, xoptions=gtk.FILL)
+
+        # Plot Legend #########################################################
 
         # Legend title.
         tlegtitle = gtk.Button("Legend title:")
-        self.gridbox.attach(tlegtitle, 0, 1, 10, 11, xoptions=gtk.FILL)
         # Text box to alter title.
         self.legtitle = gtk.Entry()
         self.legtitle.set_text("")
-        self.gridbox.attach(self.legtitle, 1, 2, 10, 11, xoptions=gtk.FILL)
 
         # Legend position.
         tlegpos = gtk.Button("Legend position:")
-        self.gridbox.attach(tlegpos, 0, 1, 11, 12, xoptions=gtk.FILL)
-        # Combo box to select position.
         self.legpos = gtk.combo_box_new_text()
-        self.gridbox.attach(self.legpos, 1, 2, 11, 12, xoptions=gtk.FILL)
         for loc in [
-                "best", "upper right", "lower left", "lower right", "right", "center left",
-                "center right", "lower center", "upper center", "center"]:
+                "best", "upper right", "lower left", "lower right", "right",
+                "center left", "center right", "lower center", "upper center",
+                "center", "no legend"]:
             self.legpos.append_text(loc)
         self.legpos.set_active(0)
 
-        #######################################################################
+        # Bins Per Dimension ##################################################
 
-        # Number of bins per dimension.
         tbins = gtk.Button("Bins per dimension:")
-        self.gridbox.attach(tbins, 0, 1, 12, 13, xoptions=gtk.FILL)
-        # Text box to alter bins.
         self.bins = gtk.SpinButton()
         self.bins.set_increments(1, 5)
         self.bins.set_range(5, 100)
         self.bins.set_value(default("nbins"))
-        self.gridbox.attach(self.bins, 1, 2, 12, 13, xoptions=gtk.FILL)
 
-        #######################################################################
+        # Plot and Bin Limits #################################################
 
         # Axes limits!
         alimits = gtk.Button(
                 "Comma separated plot limits\nx_min, x_max, y_min, y_max:")
-        self.gridbox.attach(alimits, 0, 1, 13, 14, xoptions=gtk.FILL)
         # Text box to alter title.
         self.alimits = gtk.Entry()
-        self.gridbox.attach(self.alimits, 1, 2, 13, 14, xoptions=gtk.FILL)
         self.alimits.connect("changed", self._calimits)
         self.alimits.append_text("")
 
         # Bin limits!
         blimits = gtk.Button(
                 "Comma separated bin limits\nx_min, x_max, y_min, y_max:")
-        self.gridbox.attach(blimits, 0, 1, 14, 15, xoptions=gtk.FILL)
         # Text box to alter title.
         self.blimits = gtk.Entry()
-        self.gridbox.attach(self.blimits, 1, 2, 14, 15, xoptions=gtk.FILL)
         self.blimits.connect("changed", self._cblimits)
         self.blimits.append_text("")
 
-        #######################################################################
+        # Make Plot Button ####################################################
 
-        # Button to make the plot.
         makeplot = gtk.Button('Make plot.')
         makeplot.connect("clicked", self._pmakeplot)
-        self.gridbox.attach(makeplot, 1, 2, 15, 16, xoptions=gtk.FILL)
 
-        #######################################################################
+        # Save Plot Options ###################################################
 
-        # Check boxes to control what is saved (note we only attach them to the window
-        # after showing a plot).
+        # Check boxes to control what is saved (note we only attach them to the
+        # window after showing a plot).
         self.save_pdf = gtk.CheckButton('Save PDF')
         self.save_summary = gtk.CheckButton('Save summary .txt')
         self.save_pickle = gtk.CheckButton('Save .pkl')
 
-        # Show all boxes so far regardless.
+        # Layout - GTK Table ##################################################
 
+        self.gridbox = gtk.Table(16, 5, False)
+
+        self.gridbox.attach(typetitle, 0, 1, 0, 1, xoptions=gtk.FILL)
+        self.gridbox.attach(self.typebox, 1, 2, 0, 1, xoptions=gtk.FILL)
+
+        self.gridbox.attach(xtitle, 0, 1, 1, 2, xoptions=gtk.FILL)
+        self.gridbox.attach(self.x, 1, 2, 1, 2, xoptions=gtk.FILL)
+        self.gridbox.attach(self.xtext, 1, 2, 2, 3, xoptions=gtk.FILL)
+
+        self.gridbox.attach(ytitle, 0, 1, 3, 4, xoptions=gtk.FILL)
+        self.gridbox.attach(self.y, 1, 2, 3, 4, xoptions=gtk.FILL)
+        self.gridbox.attach(self.ytext, 1, 2, 4, 5, xoptions=gtk.FILL)
+
+        self.gridbox.attach(ztitle, 0, 1, 5, 6, xoptions=gtk.FILL)
+        self.gridbox.attach(self.z, 1, 2, 5, 6, xoptions=gtk.FILL)
+        self.gridbox.attach(self.ztext, 1, 2, 6, 7, xoptions=gtk.FILL)
+
+        self.gridbox.attach(self.logx, 0, 1, 2, 3, xoptions=gtk.FILL)
+        self.gridbox.attach(self.logy, 0, 1, 4, 5, xoptions=gtk.FILL)
+        self.gridbox.attach(self.logz, 0, 1, 6, 7, xoptions=gtk.FILL)
+
+        self.gridbox.attach(tplottitle, 0, 1, 9, 10, xoptions=gtk.FILL)
+        self.gridbox.attach(self.plottitle, 1, 2, 9, 10, xoptions=gtk.FILL)
+
+        self.gridbox.attach(tlegtitle, 0, 1, 10, 11, xoptions=gtk.FILL)
+        self.gridbox.attach(self.legtitle, 1, 2, 10, 11, xoptions=gtk.FILL)
+
+        self.gridbox.attach(tlegpos, 0, 1, 11, 12, xoptions=gtk.FILL)
+        self.gridbox.attach(self.legpos, 1, 2, 11, 12, xoptions=gtk.FILL)
+
+        self.gridbox.attach(tbins, 0, 1, 12, 13, xoptions=gtk.FILL)
+        self.gridbox.attach(self.bins, 1, 2, 12, 13, xoptions=gtk.FILL)
+
+        self.gridbox.attach(alimits, 0, 1, 13, 14, xoptions=gtk.FILL)
+        self.gridbox.attach(self.alimits, 1, 2, 13, 14, xoptions=gtk.FILL)
+
+        self.gridbox.attach(blimits, 0, 1, 14, 15, xoptions=gtk.FILL)
+        self.gridbox.attach(self.blimits, 1, 2, 14, 15, xoptions=gtk.FILL)
+
+        self.gridbox.attach(makeplot, 1, 2, 15, 16, xoptions=gtk.FILL)
+
+        # Make main GUI window.
+        self.window = gtk.Window()
+        self.window.maximize()  # Window is maximised.
+        self.window.set_title("SuperPlot")  # With this title in the header.
+        self.window.connect(
+                'destroy',
+                lambda w: gtk.main_quit())  # Quit when we press cross.
+
+        # Add the table to the window and show
+        self.window.add(self.gridbox)
+        self.gridbox.show()
         self.window.show_all()
 
         return
 
-    ##########################################################################
-
+    ###########################################################################
     # Call-back functions. These functions are executed when buttons
     # are pressed/options selected. The get_active returns the index
     # rather than the label of the option selected. We find the data key

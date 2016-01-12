f3f9c29bd4e88f9672b08b57b44e2a56e4708a3b
michaelhb <mhbardsley@gmail.com>
2016-01-12 17:44:40 +1100

    added logic to setup.py which will hopefully allow pygtk to be pulled in on windows, without the linux install blowing up

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+from setuptools import setup, find_packages
 import os
+import sys
+import platform
 
 # Download setuptools if it's not installed on target system
 import ez_setup
 ez_setup.use_setuptools()
-
-from setuptools import setup, find_packages
 
 
 # Utility function to read the README file.
@@ -14,19 +15,41 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+requires = [
+    "prettytable",
+    "simpleyaml",
+    "numpy",
+    "matplotlib",
+    "scipy",
+    "pandas"
+]
+
+# install_requires should contain pygtk. However there's a problem,
+# at least on linux:
+# - pygtk can't be build from setuptools (only possible on Windows)
+# - setuptools isn't smart enough to figure out that the module is already
+#   there - i.e. it'll try to install it, remember it can't, and die.
+
+if platform.system() == "Windows":
+    # So if we're on windows, we can add it to the list.
+    requires.append("pygtk")
+else:
+    # Otherwise the best we can do is tell the user to install pygtk
+    # before trying again.
+    try:
+        import pygtk
+    except ImportError:
+        print "Superplot requires PyGTK, which is not available on this system " \
+              "and cannot be automatically installed. Please install PyGTK " \
+              "version 2.0 or greater using your OS package manager or via " \
+              "www.pygtk.org"
+        sys.exit(1)
+
 
 setup(
         setup_requires=["setuptools_git"],
 
-        install_requires=[
-            "prettytable",
-            "simpleyaml",
-            "numpy",
-            "matplotlib",
-            "scipy",
-            "pandas",
-            "pygtk"
-        ],
+        install_requires=requires,
 
         packages=["superplot"],
         include_package_data=True,

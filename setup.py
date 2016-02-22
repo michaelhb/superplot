a50bbab9e785ce7c2b8df4e209710c99d454f1e4
michaelhb <mhbardsley@gmail.com>
2016-02-22 16:16:38 +1100

    Public release 1.0.6

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ setup(
         include_package_data=True,
 
         name="superplot",
-        version="1.0.6-testing",
+        version="1.0.6",
         author="Andrew Fowlie, Michael Bardsley",
         author_email="mhbar3@student.monash.edu",
         license="GPL v2",

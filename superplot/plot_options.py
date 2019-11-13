b8598eb8f20004beebbed544c8213b70520f5113
Andrew Fowlie <andrew.j.fowlie@qq.com>
2019-11-13 10:29:19 +0800

    error checking on loading yamls

--- superplot/plot_options.py
+++ superplot/plot_options.py
@@ -1,27 +1,36 @@
 """
-Loads default settings from config.yml and makes them available.
+Loads default settings from a yaml and makes them available.
 """
 
+import warnings
 import numpy as np
 from data_loader import load_yaml
  
-converters = {"alpha": lambda x: np.sort(np.array(x)),
-              "plot_title": lambda x: "" if x is None else x}   
 
-class Defaults(dict):
+# Converters for data in yaml
+converters = {"alpha": lambda x: np.sort(np.array(x)),
+              "plot_title": lambda x: "" if x is None else x}
+
+class Defaults(object):
+    """
+    Retrieve data from a YAML or modify keys.
+    """
     def __init__(self, yaml_file):
-        self._yaml_file = yaml_file
+        self.__dict__['_yaml_file'] = yaml_file
     def __setattr__(self, attr, value):
-        self[attr] = value
+        if not hasattr(self, attr):
+             warnings.warn("Setting option {} but cannot find it in defaults".format(attr))
+        self.__dict__[attr] = value
     def __getattr__(self, attr):
         try:
-            return self[attr]
-        except:
+            return self.__dict__[attr]
+        except KeyError:
             yaml = load_yaml(self._yaml_file)
             c = converters.get(attr, lambda x: x)
             try:
                 return c(yaml[attr])
-            except:
-                raise AttributeError
+            except KeyError as m:
+                raise AttributeError("Cannot find option {} in defaults".format(attr))
+
 
 defaults = Defaults("options.yml")
--- superplot/schemes.py
+++ superplot/schemes.py
@@ -3,21 +3,34 @@ This module contains the Scheme class, which is used to hold information
 about how individual elements should appear in a plot.
 """
 
+import warnings
 from data_loader import load_yaml
 
 
 class Scheme(object):
+    """
+    Fetch data about a scheme from a yaml.
+    """
     def __init__(self, name, yaml_file):
         self._yaml_file = yaml_file 
         self._name = name
     def __getattr__(self, attr):
         yaml = load_yaml(self._yaml_file)
         try:
-            return yaml[self._name][attr]
+            block = yaml[self._name]
         except:
+            warnings.warn("No {0} - using None for {0}.{1}".format(self._name, attr))
+
+        try:
+            return block[attr]
+        except:
+            warnings.warn("No {1} - using None for {0}.{1}".format(self._name, attr))
             return None
 
 class Schemes(object):
+    """
+    Organize all schemes in a yaml.
+    """
     def __init__(self, yaml_file):
         self._yaml_file = yaml_file
     def __getattr__(self, attr):
